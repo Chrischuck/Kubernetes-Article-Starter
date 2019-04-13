4ed1ff09203a608aeee5efbaa864620d4ed17ef7
Chris <chrischuck35@gmail.com>
2019-04-13 13:02:22 -0700

    Adds basic creation for workout

--- client/src/app/components/create.js
+++ client/src/app/components/create.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react'
 
 export default class extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: '',
+      text: ''
+    }
+  }
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value })
+
+  onClick = async () => {
+    const { text, title } = this.state
+    const { pushWorkout } = this.props
+
+    const workout = {
+      title,
+      text,
+      date: new Date()
+    }
+    
+    pushWorkout(workout)
+    this.setState({ workout: '', title: '' })
+  }
+
   render() {
+    const { workout, title } = this.state
     return (
-      <div className="card" style={{ height: '320px'}}>
+      <div className="card" style={{ height: '350px'}}>
         <h1>Log a Workout</h1>
-        <textarea style={{width: '100%', height: '150px'}} />
-        <button className="button">Log!</button>
+        <input
+          name="title"
+          placeholder="Title"
+          value={title}
+          style={{width: '100%', height: '20px', fontSize: '16px', margin: '5px 0'}}
+          onChange={this.onChange}
+        />
+        <textarea
+          name="text"
+          placeholder="Workout notes"
+          value={workout}
+          style={{width: '100%', height: '150px'}}
+          onChange={this.onChange}
+        />
+        <button className="button" onClick={this.onClick}>Log!</button>
       </div>
     )
   }
--- client/src/app/index.js
+++ client/src/app/index.js
@@ -6,13 +6,25 @@ import Create from './components/create'
 import './index.css'
 
 export default class extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      workouts: []
+    }
+  }
+
+  pushWorkout = (workout) => {
+    this.setState(state => ({ workouts: [workout, ...state.workouts] }))
+  }
+
   render() {
     return (
       <Fragment>
         <Header />
         <div className="content-container">
           <div className="content">
-            <Create />
+            <Create pushWorkout={this.pushWorkout} />
           </div>
         </div>
       </Fragment>
