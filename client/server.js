ed4e0d645388e9f1168335e9b5f21cc8c27ac4f0
Chris <chrischuck35@gmail.com>
2019-04-14 15:03:51 -0700

    Creates basic starter app

---
+++ client/server.js
@@ -0,0 +1,22 @@
+
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', function(req, res) {
+    res.sendfile('./dist/index.html');
+});
+
+
+app.listen(PORT, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info("❤️  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  }
+});
